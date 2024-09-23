9fee56bf582717f6ebe040225c696e53b0d044ae
feliperieder <feliperieder@gmail.com>
2024-09-23 11:14:00 -0300

    Sistema de criação de fases melhorado

---
+++ level_line.gd
@@ -0,0 +1,55 @@
+extends Line2D
+
+
+const OBJECT_SCENE_PATH = preload("res://Points/Positive Points.tscn")
+const SPACING = 1  # Espaçamento entre os objetos
+
+@export_enum("red", "yellow", "blue", "white", "purple", "pink", "light_yellow", "green") var color = "red"
+const COLOR_RED = Color.RED
+const COLOR_YELLOW = Color.YELLOW
+const COLOR_BLUE = Color.BLUE
+const COLOR_WHITE = Color.WHITE
+const COLOR_PURPLE = Color.PURPLE
+const COLOR_PINK = Color.PINK
+const COLOR_GREEN = Color.GREEN
+const COLOR_LIGHT_YELLOW = Color.LIGHT_GOLDENROD
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	setColor()
+	print()
+	fill_line_with_objects()
+	var bullets = get_tree().get_node_count_in_group("positive_point")
+	var player = get_tree().get_first_node_in_group("brush")
+	player.current_bullets = bullets
+	player.bullet_point = player.MAX_POINTS / bullets
+
+# Função para preencher a linha com objetos
+func fill_line_with_objects() -> void:
+	for i in range(points.size() - 1):
+		var start_point = points[i]
+		var end_point = points[i + 1]
+		var distance = start_point.distance_to(end_point)
+
+		# Calcular quantos objetos instanciar entre os pontos
+		var num_objects = int(distance / SPACING)
+
+		for j in range(num_objects):
+			# Calcular a posição usando linear_interpolate
+			var position = start_point.lerp(end_point, j / float(num_objects))
+			var instance = OBJECT_SCENE_PATH.instantiate()
+			instance.color = color
+			instance.position = position
+			add_child(instance)  # Adiciona o objeto à cena
+
+func setColor():
+	match color:
+		"red": default_color = COLOR_RED
+		"yellow": default_color = COLOR_YELLOW
+		"blue": default_color = COLOR_BLUE
+		"white": default_color = COLOR_WHITE
+		"purple": default_color = COLOR_PURPLE
+		"pink": default_color = COLOR_PINK
+		"light_yellow": default_color = COLOR_LIGHT_YELLOW
+		"green": default_color = COLOR_GREEN
+		
--- main.gd
+++ main.gd
@@ -1,44 +1,17 @@
 extends Node2D
 var points
 
-@onready var background = $Line2D as Line2D
+@onready var background = $LevelLine as Line2D
 @onready var clock = $Timer as Node2D
 @onready var player = $PlayerTest as CharacterBody2D
 @onready var drawing = $Drawing as Node2D
 
 const SCORE_MENU_PATH = preload("res://Menus/Score Menu.tscn")
 
-# Caminho para a cena do objeto que você deseja instanciar
-const OBJECT_SCENE_PATH = preload("res://Points/Positive Points.tscn")
-const SPACING = 1  # Espaçamento entre os objetos
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-		fill_line_with_objects()
-		var bullets = get_tree().get_node_count_in_group("positive_point")
-		player.current_bullets = bullets
-		player.bullet_point = player.MAX_POINTS / bullets
-
-# Função para preencher a linha com objetos
-func fill_line_with_objects() -> void:
-	var points = background.points  # Pega os pontos da Line2D
-
-	for i in range(points.size() - 1):
-		var start_point = points[i]
-		var end_point = points[i + 1]
-		var distance = start_point.distance_to(end_point)
-
-		# Calcular quantos objetos instanciar entre os pontos
-		var num_objects = int(distance / SPACING)
-
-		for j in range(num_objects):
-			# Calcular a posição usando linear_interpolate
-			var position = start_point.lerp(end_point, j / float(num_objects))
-			var instance = OBJECT_SCENE_PATH.instantiate()
-			instance.color = "red"
-			instance.position = position
-			add_child(instance)  # Adiciona o objeto à cena
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
