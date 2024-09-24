70468b0bf72ff260c32a7a39c9a5c8a0935ebc3b
feliperieder <feliperieder@gmail.com>
2024-09-24 20:27:56 -0300

    Menu

---
+++ Menus/menu.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+const FIRST_LEVEL_PATH = "res://main.tscn"
+const CREDITS_PATH = "res://Menus/HighScoreMenu.tscn"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file(FIRST_LEVEL_PATH)
+
+
+func _on_score_pressed() -> void:
+	get_tree().change_scene_to_file(CREDITS_PATH)
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
