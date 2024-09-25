092e2e956913f108ea6c24b49df66d8d8f0958f9
feliperieder <feliperieder@gmail.com>
2024-09-24 21:44:56 -0300

    Tentando salvar lista

--- Menus/score_menu.gd
+++ Menus/score_menu.gd
@@ -9,7 +9,7 @@ var score
 var level
 var next_level
 
-const MINIMUM_SCORE = 700
+const MINIMUM_SCORE = 7000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -30,4 +30,6 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
+	if button.text == "Try Again":
+		ScoreSystem.resetScore()
 	get_tree().change_scene_to_file(next_level)
--- score_system.gd
+++ score_system.gd
@@ -1,13 +1,36 @@
 extends Node
 
-const SAVE_PATH = "res://ScoreSave/save_data.tres"
+const SAVE_PATH = "res://ScoreSave/save_data.save"
 var total_score = 0
+var top10 = [0,0,0,0,0,0,0,0,0,0]
+var top10_nick = ['a']
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	#loadScore()
+	print(top10)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func saveScore():
+	#abre o arquivo e salva nele o q está em data
+	var get_file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	top10 = get_file.get_line(top10)
+	top10_nick = get_file.get(top10_nick)
+	get_file.close()
+
+
+func loadScore():
+	if FileAccess.file_exists(SAVE_PATH):
+		var get_load = FileAccess.open(SAVE_PATH, FileAccess.READ)
+		FileAccess.open(SAVE_PATH, FileAccess.READ)
+		top10 = get_load.get(top10)
+		top10_nick = get_load.get(top10_nick)
+		get_load.close()
+
+func resetScore():
+	#ScoreSystem.top10.append(ScoreSystem.total_score)
+	#ScoreSystem.saveScore()
+	#total_score = 0
