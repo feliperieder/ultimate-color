f5e55c280e060cf9e902ce4b18b4c8850e3c27d9
feliperieder <feliperieder@gmail.com>
2024-09-26 01:26:54 -0300

    Menu art

--- Menus/menu.gd
+++ Menus/menu.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 const FIRST_LEVEL_PATH = "res://main.tscn"
 const CREDITS_PATH = "res://Menus/HighScoreMenu.tscn"
+const TUTORIAL_PATH = "res://Menus/TutorialMenu.tscn"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$CanvasLayer/VBoxContainer/HBoxContainer/Start.grab_focus()
+	$VBoxContainer/HBoxContainer/Start.grab_focus()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,3 +25,7 @@ func _on_score_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_how_to_play_pressed() -> void:
+	get_tree().change_scene_to_file(TUTORIAL_PATH)
---
+++ Menus/tutorial_menu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const MENU_PATH = "res://Menus/Menu.tscn"
+@onready var back_button = $CanvasLayer/VBoxContainer/Button as Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	back_button.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file(MENU_PATH)
